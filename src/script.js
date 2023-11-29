103407b94ce861c2634702291de3f773561bf23f
Lucas Biazon Palma <lucasbiazonpalma@gmail.com>
2023-11-29 04:25:47 -0300

    Tela inicial

--- src/script.js
+++ src/script.js
@@ -1,3 +1,5 @@
+const player1 = document.querySelector("#player1")
+const player2 = document.querySelector("#player2")
 
 const tabuleiro = document.querySelector("#tabuleiro")
 const boxs = document.querySelectorAll('.boxs')
@@ -6,12 +8,23 @@ let gameOver = false
 
 tabuleiro.addEventListener('click', Jogada)
 
+document.addEventListener('keyup', (event) =>{
+    if(event.key == "Enter"){
+        Jogar()
+    }
+})
+function Jogar(){
+    document.querySelector("#inicial").classList.add('hidden')
+    document.querySelector("#jogo").classList.remove("hidden")
+    document.querySelector("#name1").innerHTML = player1.value
+    document.querySelector("#name2").innerHTML = player2.value
+}
+
 function Jogada(event){
     if(event.target.classList.contains('boxs')){
         if(!event.target.classList.contains('x') && !event.target.classList.contains('o')){
             event.target.classList.add(turno)
-           console.log(turno)
-           event.target.textContent = turno
+            event.target.textContent = turno
             
           checarVitoria(turno)
             
@@ -23,7 +36,7 @@ function Jogada(event){
 
         
             if(gameOver){
-                console.log("O jogo acabou")
+                GameOver()
             }
 
             
@@ -61,4 +74,14 @@ function checarVitoria(valor){
             break
         }
     }
+}
+
+function GameOver(){
+    alert('Game Over')
+    for(let i of boxs){
+        i.classList.remove('x')
+        i.classList.remove('o')
+        i.textContent = ''
+    }
+    gameOver = false
 }
